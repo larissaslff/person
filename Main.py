c3c12e2121277ecff820e6cccc8838ac6b1f40f3
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-06-15 20:44:27 -0300

    add def falar

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 from Pessoa import Pessoa
 
 p1 = Pessoa('Larissa', 25, '12345689')
-print(p1)
+print(p1.falando)
+print(p1.falar('casa'))
+print(p1.falando)
+print(p1.pararDeFalar())
+print(p1.falando)
+print(p1.pararDeFalar())
--- Pessoa.py
+++ Pessoa.py
@@ -3,4 +3,17 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
         self.cpf = cpf
-        
+        self.falando = False
+
+    def falar(self, subject):
+        if not self.falando:
+            self.falando = True
+            return f'Estou falando sobre {subject}'
+
+
+    def pararDeFalar(self):
+        if self.falando:
+            self.falando = False
+            return 'Estou parando de falar'
+        else:
+            return 'Eu não posso parar, pois não estava falando'
